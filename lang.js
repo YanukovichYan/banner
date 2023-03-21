72e65120a34ddbe54e5c1101e02c192495f39da1
y.yanukovich <y.yanukovich@mail.ru>
2023-03-21 10:42:41 +0300

    [master]: added translates

--- lang.js
+++ lang.js
@@ -30,17 +30,46 @@ window.onload = () => {
     Promise.resolve(fetch(`${path}/${lang}.json`))
         .then(response => response.json())
         .then(data => {
-            processData(data)
+            processData(data, lang)
         })
 
 }
 
-function processData(data) {
+function processData(data, lang) {
     console.log(data)
 
-    if (document.getElementById('btn')) {
-        document.getElementById('btn').onclick = () => {
-            console.log('CLICk')
-        }
+    // if (document.getElementById('btn')) {
+    //     document.getElementById('btn').onclick = () => {
+    //         console.log('CLICk')
+    //     }
+    // }
+
+    if (lang === 'ru' ||  lang === 'fr') {
+        document.getElementsByClassName('free-days')[0].style.fontSize = '15px'
+        document.getElementsByClassName('free-days')[1].style.fontSize = '15px'
     }
+
+    const price = '$9.99'
+
+    document.getElementById('main-title').innerHTML = data["Unlimited Access<br>to All Features"]
+    document.getElementById('restore').innerHTML = data["Restore"]
+    document.getElementById('first-advantages').innerHTML = data["Unlimited documents"]
+    document.getElementById('second-advantages').innerHTML = data["Export to clouds"]
+    document.getElementById('third-advantages').innerHTML = data["Text recognition (OCR)"]
+
+    document.getElementsByClassName('card-title')[0].innerHTML = data["Monthly"]
+    document.getElementsByClassName('price')[0].innerHTML = data["<strong>{{price}}</strong><br>per month"]
+    document.getElementsByClassName('free-days')[0].innerHTML = data["3 DAYS FREE"]
+    document.getElementsByClassName('bottom-price')[0].innerHTML = data["{{price}}/month"]
+
+    document.getElementsByClassName('card-title')[1].innerHTML = data["Annually"]
+    document.getElementsByClassName('price')[1].innerHTML = data["<strong>{{price}}</strong><br>per year"]
+    document.getElementsByClassName('free-days')[1].innerHTML = data["MOST POPULAR"]
+    document.getElementsByClassName('bottom-price').innerHTML = data["{{price}}/month"]
+
+    document.getElementById('btn-next').innerHTML = data["Continue"]
+    document.getElementById('sub-btn-text').innerHTML = data["Auto-renewable. Cancel anytime."]
+    document.getElementById('terms').innerHTML = data["Terms of Use"]
+    document.getElementById('privacy-policy').innerHTML = data["Privacy Policy"]
+
 }
